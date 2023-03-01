9944641bb1f852c483c544af94f9b997f1309301
Spas Z. Spasov <spas@browserbite.io>
2023-03-01 17:24:46 +0200

    Lesson 6: API Routes

---
+++ pages/api/hello.js
@@ -0,0 +1,16 @@
+/**
+ * Visit: http://localhost:3000/api/hello
+ * Or in the browser's console try:
+ *
+ *  fetch("/api/hello", {method: "post"})
+ *     .then(res => res.json())
+ *     .then(data => console.log(data));
+ *
+ * Refs:
+ *  > https://nextjs.org/learn/basics/api-routes/api-routes-details
+ *  > https://nextjs.org/docs/api-routes/introduction
+ *  > https://nextjs.org/docs/advanced-features/preview-mode
+ */
+export default function handler(req, res) {
+  res.status(200).json({ text: `Hello, you are using: ${req.method}` });
+}
